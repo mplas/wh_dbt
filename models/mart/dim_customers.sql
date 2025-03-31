a20d48eeb0a6ad40cbe8e4f7ac1dd587ff7b6687
Marco van der Plas <plas@hotitem.nl>
2025-03-31 11:31:15 +0000

    bug fixed

--- models/mart/dim_customers.sql
+++ models/mart/dim_customers.sql
@@ -1,45 +1,32 @@
-with customers as (
+with
+    customers as (select * from {{ ref("stg_jaffle_shop__customers") }}),
 
-     select * from {{ ref('stg_jaffle_shop__customers') }}
+    orders as (select * from {{ ref("stg_jaffle_shop__orders") }}),
 
-),
+    customer_orders as (
 
-orders as ( 
+        select
+            customer_id,
+            min(order_date) as first_order_date,
+            max(order_date) as most_recent_order_date,
+            count(order_id) as number_of_orders
 
-    select * from {{ ref('stg_jaffle_shop__orders') }}
+        from orders
 
-),
+        group by customer_id
 
-customer_orders as (
+    ),
 
-    select
-        customer_id,
-
-        min(order_date) as first_order_date,
-        max(order_date) as most_recent_order_date,
-        count(order_id) as number_of_orders
-
-    from orders
-
-    group by 1
-
-),
-
-final as (
-
-    select
-        customers.customer_id,
-        customers.first_name,
-        customers.last_name,
-        customer_orders.first_order_date,
-        customer_orders.most_recent_order_date,
-        coalesce (customer_orders.number_of_orders, 0) 
-        as number_of_orders
-
-    from customers
-
-    left join customer_orders using (customer_id)
-
-)
-
-select * from final
+    final as (
+        select
+            cst.customer_id,
+            cst.first_name,
+            cst.last_name,
+            cst_ord.first_order_date,
+            cst_ord.most_recent_order_date,
+            coalesce(cst_ord.number_of_orders, 0) as number_of_orders
+        from customers cst
+        left join customer_orders cst_ord on cst.customer_id = cst_ord.customer_id
+    )
+select *
+from final
